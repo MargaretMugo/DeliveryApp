7678d53a427b1214a53ea199775c186cd6208fa3
vickykimani <victorianightingale70@gmail.com>
2023-01-18 21:48:49 +0300

    django suit

--- DeliveryApp/settings.py
+++ DeliveryApp/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'customer',
+    'DeliveryApp.apps.SuitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
-    'delivery',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'customer',
+    'delivery'
 ]
 
 MIDDLEWARE = [
