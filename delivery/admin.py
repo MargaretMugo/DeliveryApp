b2d9076d09075c8ceb4300e76d1bf2bed6fce4d3
vickykimani <victorianightingale70@gmail.com>
2023-01-18 19:27:47 +0300

    add delivery

--- delivery/admin.py
+++ delivery/admin.py
@@ -3,5 +3,12 @@ from django.contrib.auth.models import Group
 from .models import Snippet
 
 admin.site.site_header = 'Delivery Admin'
+
+
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created')
+    list_filter = ('created',)
+
+
 admin.site.register(Snippet)
 admin.site.unregister(Group)
--- delivery/models.py
+++ delivery/models.py
@@ -5,6 +5,7 @@ class Snippet(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    font_size = models.IntegerField()
 
     def __str__(self):
         return self.title
