007091b2a140cdbaf88332bf0daae394bac536ee
vickykimani <victorianightingale70@gmail.com>
2023-01-18 21:03:48 +0300

    add delivery

--- delivery/admin.py
+++ delivery/admin.py
@@ -8,6 +8,7 @@ admin.site.site_header = 'Delivery Admin'
 class SnippetAdmin(admin.ModelAdmin):
     list_display = ('title', 'created')
     list_filter = ('created',)
+    change_list_template = 'admin/deliveries/delivery_change_list.html'
 
 
 admin.site.register(Snippet)
