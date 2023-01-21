1d2019faa969df464f9dabe57bcf7fc8e80bdbde
vickykimani <victorianightingale70@gmail.com>
2023-01-21 14:46:07 +0300

    customer admin

--- customer/models.py
+++ customer/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
 
-class Delivery(models.Model):
-    title = models.CharField(max_length=100)
-    body = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+class Customer(models.Model):
+    first_name = models.CharField(max_length=300)
+    last_name = models.CharField(max_length=30)
+    delivery_name = models.CharField(max_length=30)
+    quantity = models.IntegerField()
 
     def __str__(self):
-        return self.title
+        return self.first_name
+
+    def __str__(self):
+        return self.last_name
 
     def body_preview(self):
-        return self.body[:50]
+        return self.delivery_name[:50]
--- delivery/models.py
+++ delivery/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Delivery(models.Model):
-    delivery_name = models.CharField(max_length=100)
+    delivery_name = models.CharField(max_length=30)
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     quantity = models.IntegerField()
